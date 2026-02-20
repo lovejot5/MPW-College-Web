0121de5b8309c571dcb19cf53ff87c6dbee6b4ce
Lovejot <140100951+lovejot5@users.noreply.github.com>
2026-02-21 04:39:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleMenu() {
         menu.style.display = "flex";
     }
 }
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    const btn = document.getElementById("menuBtn");
+
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+        btn.innerHTML = "☰";   // hamburger
+    } else {
+        menu.style.display = "flex";
+        btn.innerHTML = "✕";   // cross
+    }
+}
